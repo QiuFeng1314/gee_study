06bb6a06f2db58d19e032dc64ea382c89ee4dd26
QiuFeng1314 <jingshuai@petalmail.com>
2023-02-24 21:53:38 +0800

    完善main测试案例

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 func main() {
 	app := gee.New()
 
-	app.Get("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	app.Get("/", func(ctx *gee.Context) {
+		ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	app.Get("/hello", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	app.Get("/hello", func(ctx *gee.Context) {
+		ctx.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	app.Post("/login", func(ctx *gee.Context) {
+		ctx.JSON(http.StatusOK, gee.H{
+			"username": ctx.PostForm("username"),
+			"password": ctx.PostForm("password"),
+		})
 	})
 
 	app.Run(":8080")
